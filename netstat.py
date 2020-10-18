2b08d423d43d4de8e296b35444d9a416a1ed889b
Swim Systems <32812027+swimsystems@users.noreply.github.com>
2020-10-18 11:44:42 +0200

    netstat: Resolve kernel.rpc

--- netstat.py
+++ netstat.py
@@ -2,8 +2,10 @@
 
 """Show openend ports (netstat -nlp) and their associated processes."""
 
-import types
+import re
 import socket
+import subprocess
+import types
 
 import psutil
 
@@ -15,6 +17,37 @@ PROTO_MAP = {
     (socket.AF_INET, socket.SOCK_DGRAM): "udp",
     (AF_INET6, socket.SOCK_DGRAM): "udp6",
 }
+
+
+def kernel_process_by_port(proto, address):
+    """Find open kernel RPC port (rpcinfo)."""
+    result = "?"
+    try:
+        rpcinfo_output = subprocess.run(["rpcinfo"], stdout=subprocess.PIPE, check=True)
+    except FileNotFoundError:
+        return result
+    lines = rpcinfo_output.stdout.decode("ascii").splitlines()[:-1]
+    regex = re.compile(
+        r"\s+(?:\d+)\s+(?:\d)\s+(\w+)\s+" r"([.:0-9]+)\.(\d+)\.(\d+)\s+(\w+)\s+(?:\w+)"
+    )
+    processes = []
+    for line in lines:
+        match = regex.match(line)
+        if match:
+            processes.append(
+                (
+                    match.group(1),
+                    "{}:{}".format(
+                        match.group(2), 256 * int(match.group(3)) + int(match.group(4))
+                    ),
+                    match.group(5),
+                )
+            )
+
+    for item in processes:
+        if item[0] == proto and item[1] == address:
+            return "rpc.{}".format(item[2])
+    return result
 
 
 def process_names():
@@ -43,7 +76,11 @@ def main():
         addr = connection.laddr[0]
         port = connection.laddr[1]
         proto = PROTO_MAP[(connection.family, connection.type)]
-        proc = proc_names[connection.pid] if connection.pid else "kernel-rpc"
+        proc = (
+            proc_names[connection.pid]
+            if connection.pid
+            else kernel_process_by_port(proto, "{}:{}".format(addr, port))
+        )
         print(templ % (proto, addr, port, connection.pid or "-", proc))
 
 
